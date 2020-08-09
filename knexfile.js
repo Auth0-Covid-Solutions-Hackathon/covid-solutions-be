37ffb93d9ca191bb8a0d0255682fac0c75f6f59a
Muamer Kukic <kukicako@gmail.com>
2020-08-09 06:30:23 -0700

    pg unsupport

--- knexfile.js
+++ knexfile.js
@@ -14,30 +14,4 @@ module.exports = {
     },
   },
 
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    },
-  },
-
-  testing: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/dev.sqlite3',
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    },
-  },
-
-
 };
