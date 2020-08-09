b9c1eda1fb2de20dc4954dae67b28e01fe0af8c9
Muamer Kukic <kukicako@gmail.com>
2020-08-09 03:20:27 -0700

    seed test

--- data/seeds/001-companies.js
+++ data/seeds/001-companies.js
@@ -5,12 +5,12 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('companies').insert([
-        {id: 1, name: 'Walmart', image:"https://seeklogo.com/images/W/Walmart-logo-B6CAC8AF23-seeklogo.com.png", capacity: "false", distance: "true", masks: "true", home: "false", elder: "true"  },
-        {id: 2, name: 'Amazon',image:"https://seeklogo.com/images/A/amazon-logo-86547BFB54-seeklogo.com.png", capacity: "false", distance: "false", masks: "true", home: "false", elder: "false" },
-        {id: 3, name: 'Nike',image:"https://cdn4.iconfinder.com/data/icons/flat-brand-logo-2/512/nike-512.png", capacity: "false", distance: "true", masks: "false", home: "true", elder: "false" },
+        {id: 1, name: 'Walmart', image:"https://img.icons8.com/color/480/000000/walmart.png", capacity: "false", distance: "true", masks: "true", home: "false", elder: "true"  },
+        {id: 2, name: 'Amazon',image:"https://cdn2.iconfinder.com/data/icons/social-icons-color/512/amazon-256.png", capacity: "false", distance: "false", masks: "true", home: "false", elder: "false" },
+        {id: 3, name: 'Nike',image:"https://img.icons8.com/color/480/000000/nike.png", capacity: "false", distance: "true", masks: "false", home: "true", elder: "false" },
         {id: 4, name: 'Kroger',image:"https://cdn4.iconfinder.com/data/icons/social-media-2273/64/social_media_network_online_k-256.png", capacity: "false", distance: "false", masks: "false", home: "false", elder: "false" },
-        {id: 5, name: 'McDonalds',image:"http://www.simpleimageresizer.com/_uploads/photos/718b9342/mcDees_35.png", capacity: "false", distance: "false", masks: "false", home: "false", elder: "false" },
-        {id: 6, name: 'Burlington',image:"http://www.simpleimageresizer.com/_uploads/photos/718b9342/jeffrey_35.png", capacity: "true", distance: "false", masks: "true", home: "false", elder: "true" },
+        {id: 5, name: 'McDonalds',image:"http://www.simpleimageresizer.com/_uploads/photos/718b9342/mcDees_5_25.png", capacity: "false", distance: "false", masks: "false", home: "false", elder: "false" },
+        {id: 6, name: 'Burlington',image:"http://www.simpleimageresizer.com/_uploads/photos/718b9342/jeffrey_1_25.png", capacity: "true", distance: "false", masks: "true", home: "false", elder: "true" },
         {id: 7, name: 'Target',image:"https://cdn3.iconfinder.com/data/icons/shopping-and-retail-15/512/Focus-512.png", capacity: "true", distance: "true", masks: "true", home: "true", elder: "true" },
         {id: 8, name: 'Adidas', image:"https://cdn4.iconfinder.com/data/icons/logos-and-brands-1/512/5_Adidas_logo_logos-512.png", capacity: "true", distance: "true", masks: "false", home:"false", elder: "false" },
 
