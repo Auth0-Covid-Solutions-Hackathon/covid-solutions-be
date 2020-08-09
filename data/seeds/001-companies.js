a983464f8e8ae17402d8bc8ba36f6d2fa9c529b7
Muamer Kukic <kukicako@gmail.com>
2020-08-09 02:23:05 -0700

    image resize

--- data/seeds/001-companies.js
+++ data/seeds/001-companies.js
@@ -9,10 +9,10 @@ exports.seed = function(knex) {
         {id: 2, name: 'Amazon',image:"https://seeklogo.com/images/A/amazon-logo-86547BFB54-seeklogo.com.png", capacity: "false", distance: "false", masks: "true", home: "false", elder: "false" },
         {id: 3, name: 'Nike',image:"https://cdn4.iconfinder.com/data/icons/flat-brand-logo-2/512/nike-512.png", capacity: "false", distance: "true", masks: "false", home: "true", elder: "false" },
         {id: 4, name: 'Kroger',image:"https://cdn4.iconfinder.com/data/icons/social-media-2273/64/social_media_network_online_k-256.png", capacity: "false", distance: "false", masks: "false", home: "false", elder: "false" },
-        {id: 5, name: 'McDonalds',image:"https://cdn4.iconfinder.com/data/icons/flat-brand-logo-2/512/mcdonalds-512.png", capacity: "false", distance: "false", masks: "false", home: "false", elder: "false" },
-        {id: 6, name: 'Burlington',image:"https://cdn4.iconfinder.com/data/icons/aiga-symbol-signs/612/aiga_coat_check_bg-512.png", capacity: "true", distance: "false", masks: "true", home: "false", elder: "true" },
+        {id: 5, name: 'McDonalds',image:"http://www.simpleimageresizer.com/_uploads/photos/718b9342/mcDees_35.png", capacity: "false", distance: "false", masks: "false", home: "false", elder: "false" },
+        {id: 6, name: 'Burlington',image:"http://www.simpleimageresizer.com/_uploads/photos/718b9342/jeffrey_35.png", capacity: "true", distance: "false", masks: "true", home: "false", elder: "true" },
         {id: 7, name: 'Target',image:"https://cdn3.iconfinder.com/data/icons/shopping-and-retail-15/512/Focus-512.png", capacity: "true", distance: "true", masks: "true", home: "true", elder: "true" },
-        {id: 8, name: 'Adidas',image:"https://cdn4.iconfinder.com/data/icons/logos-and-brands-1/512/5_Adidas_logo_logos-512.png", capacity: "true", distance: "true", masks: "false", home: "false", elder: "false" },
+        {id: 8, name: 'Adidas', image:"https://cdn4.iconfinder.com/data/icons/logos-and-brands-1/512/5_Adidas_logo_logos-512.png", capacity: "true", distance: "true", masks: "false", home:"false", elder: "false" },
 
       ]);
     });
